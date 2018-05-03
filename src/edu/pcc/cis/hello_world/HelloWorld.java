cd55846449e512a1eb8d4211af3696ca4a49e99c
Chris Jacobs <chris.jacobs2@pcc.edu>
2018-05-02 17:41:03 -0700

    Added main method that prints Hello World

--- src/edu/pcc/cis/hello_world/HelloWorld.java
+++ src/edu/pcc/cis/hello_world/HelloWorld.java
@@ -1,4 +1,18 @@
 package edu.pcc.cis.hello_world;
 
+
+/**
+ * @author Chris Jacobs
+ * @version 05.02.2018
+ *
+ * A simple program that prints hello.
+ *
+ */
 public class HelloWorld {
+
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
 }
